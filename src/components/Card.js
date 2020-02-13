c4fe431393a0972bb28bc794ed96ee49ad555d25
LisaVerhoeven <lisaverhoeven2001@hotmail.com>
2020-02-13 16:35:23 +0100

    adding files to repo

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React,{ Fragment } from 'react';
+
+
+const Card = ({ name, email, id }) => {
+	
+	return (
+		<Fragment>
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+		</Fragment>
+	)
+}
+
+export default Card;
